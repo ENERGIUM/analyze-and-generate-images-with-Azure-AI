04bf803044e0319d0427c6feb0ab8cc2d78c3b24
ENERGIUM <martintabuss@gmail.com>
2023-11-29 14:54:50 -0300

    generate descripcion image

--- src/App.js
+++ src/App.js
@@ -1,8 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
+import AnalyzeImage from './azure-image-anlysis';
 
 function App() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
+  const [imageUrl, setImageUrl] = useState('');
+  const [analysisResult, setAnalysisResult] = useState(null);
+
+  const handleAnalysisResult = (result) => {
+    setAnalysisResult(result);
+  };
+
+  return (
+    <div className="App">
+      <h1>Computer Vision Analysis</h1>
+      
+      {/* Set the image URL */}
+      <input
+        type="text"
+        placeholder="Enter Image URL"
+        value={imageUrl}
+        onChange={(e) => setImageUrl(e.target.value)}
+      />
+      
+      {/* Render AnalyzeImage component */}
+      <AnalyzeImage imageUrl={imageUrl} onAnalysisResult={handleAnalysisResult} />
+
+      {/* {analysisResult && (
+        <div>
+          <h2>Analysis Result:</h2>
+          <pre>{JSON.stringify(analysisResult, null, 2)}</pre>
+        </div>
+      )} */}
+    </div>
+  );
 }
 
 export default App;
+
---
+++ src/azure-image-anlysis.js
@@ -0,0 +1,55 @@
+import React, { useState } from 'react';
+
+const AnalyzeImage = ({ imageUrl, onAnalysisResult }) => {
+  const [result, setResult] = useState(null);
+
+  const handleAnalysis = async () => {
+    try {
+      const endpoint = '';
+      const subscriptionKey = '';
+      //&features=Tags
+      const url = `${endpoint}/computervision/imageanalysis:analyze?api-version=2023-02-01-preview&language=en&features=caption`;
+      const response = await fetch(url, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Ocp-Apim-Subscription-Key': subscriptionKey,
+        },
+        params: {
+          'visualFeatures': 'Categories,Description,Color',
+          'language': 'en',
+        },
+        body: JSON.stringify({ url: imageUrl }),
+      });
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+
+      const data = await response.json();
+      setResult(data);
+
+      // Pass the result back to the parent component
+      onAnalysisResult(data);
+    } catch (error) {
+      console.error('Error calling Azure Vision API:', error.message);
+    }
+  };
+
+  return (
+    <div>
+      <button onClick={handleAnalysis}>Analyze Image</button>
+      <h3>Selected Image:</h3>
+      {imageUrl && <img src={imageUrl} alt="Selected" style={{ maxWidth: '25%' }} />}
+      <h3>Analysis Result:</h3>
+      {result && (
+        <div>
+          <h4>Image Description:</h4>
+          <pre>{JSON.stringify(result, null, 2)}</pre>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default AnalyzeImage;
